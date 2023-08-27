1374307a0e6d65a14c37fa42b12851a98b691f16
devruso <jamilsonjr@live.com>
2023-08-27 14:25:45 -0300

    parte da estrutura da classe automovel

--- src/lista_1/Automovel.java
+++ src/lista_1/Automovel.java
@@ -1,4 +1,22 @@
 package lista_1;
 
 public class Automovel {
+
+    private String combustivel;
+    private String fabricante;
+    private int ano;
+    private String modelo;
+
+    public Automovel(String combustivel, String fabricante, int ano,  String modelo){
+        this.combustivel = combustivel;
+        this.fabricante = fabricante;
+        this.ano = ano;
+        this.modelo = modelo;
+    }
+
+    public int percorrePercurso(String combustivel, int litros){
+        return 0;
+    }
+
+
 }
