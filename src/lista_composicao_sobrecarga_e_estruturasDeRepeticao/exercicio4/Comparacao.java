59bc25b530326d9406f46d185a2540a9f09cb2f2
devruso <jamilsonjr@live.com>
2023-09-18 10:49:02 -0300

    exercicio 4

--- src/lista_composicao_sobrecarga_e_estruturasDeRepeticao/exercicio4/Comparacao.java
+++ src/lista_composicao_sobrecarga_e_estruturasDeRepeticao/exercicio4/Comparacao.java
@@ -1,2 +1,26 @@
-package lista_composicao_sobrecarga_e_estruturasDeRepeticao.exercicio4;public class Comparacao {
+package lista_composicao_sobrecarga_e_estruturasDeRepeticao.exercicio4;
+
+public class Comparacao {
+
+    private int valor;
+
+    public Comparacao(int valor){
+        this.valor = valor;
+    }
+
+    public boolean compararValor(int numero){
+        return this.valor == numero;
+    }
+
+    public boolean compararDoisNumeros(int numero1, int numero2){
+        return numero1 == numero2;
+    }
+
+    public boolean compararSeMenor(int numero){
+        return numero < this.valor;
+    }
+
+    public boolean primeiroMaior(int numero1, int numero2){
+        return numero1 > numero2;
+    }
 }
