5a1c443781e0b34dd6b78e77a2079a3c0b58995a
devruso <jamilsonjr@live.com>
2023-09-18 10:48:50 -0300

    exercicio 3

--- src/lista_composicao_sobrecarga_e_estruturasDeRepeticao/exercicio3/Cliente.java
+++ src/lista_composicao_sobrecarga_e_estruturasDeRepeticao/exercicio3/Cliente.java
@@ -1,2 +1,22 @@
-package lista_composicao_sobrecarga_e_estruturasDeRepeticao.exercicio3;public class Cliente {
+package lista_composicao_sobrecarga_e_estruturasDeRepeticao.exercicio3;
+
+public class Cliente {
+
+    private String nome;
+    private int idade;
+
+    public Cliente(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void comprarIngresso(String dia_semana){
+        if(this.idade <= 14 || this.idade >= 65 || dia_semana.equals("quarta-feira") || dia_semana.equals("quinta_feira")){
+            System.out.println("Meia entrada disponível, valor do ingresso: R$ 8,00");
+        }else{
+            System.out.println("Meia entrada indisponível, valor do ingresso: R$ 16,00");
+
+        }
+    }
+
 }
