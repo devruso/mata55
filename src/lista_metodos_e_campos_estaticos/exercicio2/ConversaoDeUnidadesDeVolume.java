db1a417fbd907bbfc0a6be5ec270f0e83e1ac120
devruso <jamilsonjr@live.com>
2023-10-09 14:49:49 -0300

    exercicio 3

---
+++ src/lista_metodos_e_campos_estaticos/exercicio2/ConversaoDeUnidadesDeVolume.java
@@ -0,0 +1,25 @@
+package lista_metodos_e_campos_estaticos.exercicio2;
+
+public class ConversaoDeUnidadesDeVolume {
+
+    static double litroParaCentimetroCubico(int n){
+        return n * 1000;
+    }
+
+    static double metroCubicoParaLitro(int n){
+        return n * 1000;
+    }
+
+    static double metroCubicoParaPesCubico(int n){
+        return n * 35.32;
+    }
+
+    static double galaoAmericanoParaPolegadaCubica(int n){
+        return n * 231;
+    }
+
+    static double galaoAmericanoParaLitros(int n){
+        return n * 3785;
+    }
+
+}
