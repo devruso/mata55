1e8982cc1ce635edbb57c7c3af792ee6a7ef410e
devruso <jamilsonjr@live.com>
2023-10-09 14:55:09 -0300

    exercicio 4

---
+++ src/lista_metodos_e_campos_estaticos/exercicio2/Main.java
@@ -0,0 +1,16 @@
+package lista_metodos_e_campos_estaticos.exercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        int volumePiscinaOlimpicaEmMetrosCubicos = 1890;
+
+        double volumeEmLitros, volumeEmPesCubicos, volumeEmCentimetrosCubicos;
+
+        volumeEmLitros = ConversaoDeUnidadesDeVolume.metroCubicoParaLitro(volumePiscinaOlimpicaEmMetrosCubicos);
+        volumeEmPesCubicos = ConversaoDeUnidadesDeVolume.metroCubicoParaPesCubico(volumePiscinaOlimpicaEmMetrosCubicos);
+        volumeEmCentimetrosCubicos = ConversaoDeUnidadesDeVolume.litroParaCentimetroCubico(volumePiscinaOlimpicaEmMetrosCubicos);
+
+        System.out.println("Volume em litros: " + volumeEmLitros + " Volume em pés cúbicos: " + volumeEmPesCubicos + " Volume em Centimetros Cúbicos: " + volumeEmCentimetrosCubicos);
+
+    }
+}
