eabe63cc9d95cb32cc71c6d33d420b5f8db5439e
devruso <jamilsonjr@live.com>
2023-10-09 15:04:13 -0300

    exercicio 5

---
+++ src/lista_metodos_e_campos_estaticos/exercicio5/ConversaoDeUnidadesDeTempo.java
@@ -0,0 +1,30 @@
+package lista_metodos_e_campos_estaticos.exercicio5;
+
+public class ConversaoDeUnidadesDeTempo {
+
+    static int minutoParaSegundo(int n){
+        return n * 60;
+    }
+
+    static int horaParaMinuto(int n){
+        return n * 60;
+    }
+
+    static int diaParaHora(int n){
+        return n * 24;
+    }
+
+    static int semanaParaDia(int n){
+        return n * 7;
+    }
+
+    static int mesParaDia(int n){
+        return n * 30;
+    }
+
+    static double anoParaDia(int n){
+        return n * 365.25;
+    }
+
+
+}
