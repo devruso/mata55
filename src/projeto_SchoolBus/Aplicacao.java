153d3f3556bab2b0f3bb778ab99b8f1c4a3f6160
devruso <jamilsonjr@live.com>
2023-10-20 09:51:07 -0300

    schoolBus_feat/wip: menu da aplicacao

--- src/projeto_SchoolBus/Aplicacao.java
+++ src/projeto_SchoolBus/Aplicacao.java
@@ -1,7 +1,35 @@
 package projeto_SchoolBus;
 
+import java.util.Scanner;
+
 public class Aplicacao {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do{
+            System.out.println("Bem vindo ao menu School Bus");
+            System.out.println("1Criar Endereco");
+            System.out.println("2. Criar Aluno");
+            System.out.println("3. Criar Motorista");
+            System.out.println("4. Criar Contrato");
+            System.out.println("5. Criar Escola");
+            System.out.println("6. Criar Veículo");
+            System.out.println("7. Criar Ponto de Parada");
+            System.out.println("8. Criar Nova Rota");
+            System.out.println("9. Calcular Demanda de Rota");
+            System.out.println("10. Exibir Total de Rotas Criadas");
+            System.out.println("11. Exibir Total de Pontos de Parada Criados");
+            opcao = sc.nextInt();
+            sc.nextLine();
+            switch (opcao){
+                case 1:
+                    System.out.println();
+            }
+        }while(opcao != 12);
+
+
+
 
     }
 }
