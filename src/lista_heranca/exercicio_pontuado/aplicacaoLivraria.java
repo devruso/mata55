6b4653cf8778ba6a076034795545030e274ae497
devruso <jamilsonjr@live.com>
2023-10-25 15:03:01 -0300

    metodo de imprimir titulos

--- src/lista_heranca/exercicio_pontuado/aplicacaoLivraria.java
+++ src/lista_heranca/exercicio_pontuado/aplicacaoLivraria.java
@@ -1,7 +1,20 @@
 package lista_heranca.exercicio_pontuado;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class aplicacaoLivraria {
-    public String nome_da_livraria = "Livraria do milsinho";
+    public String nome_da_livraria = "Livraria luz do sol";
+
+    static void imprimirTitulos(){
+        ArrayList<Livro> listaLivros = new ArrayList<>();
+        ArrayList<Revista> listaRevistas = new ArrayList<>();
+
+        listaLivros.forEach(e -> e.toString());
+        listaRevistas.forEach(e -> e.toString());
+    }
+
+
 
     public static void main(String[] args) {
         Livro livro = new Livro("The witcher", "O russo lá",
