92a4b006b61878cb40dcea90b8fdfc73993bda97
devruso <jamilsonjr@live.com>
2023-10-28 13:53:35 -0300

    schoolBus_fix: correcao de atributos repetidos

--- src/projeto_SchoolBus/Aluno.java
+++ src/projeto_SchoolBus/Aluno.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 public class Aluno extends PessoaFisica
 {
-    private String cpf;
     private Escola escola;
     private int matricula;
     private int serie;
@@ -12,21 +11,14 @@ public class Aluno extends PessoaFisica
     private PontoDeParada pontoDeParada;
 
     public Aluno(String nome_oficial, String cpf_cnpj, Endereco endereco, String telefone, String nome, String mae,
-                 String pai, String naturalidade, Date data_nascimento, String cpf,
-                Escola escola, int matricula, int serie, String turno, PontoDeParada pontoDeParada) {
+                 String pai, String naturalidade, Date data_nascimento, Escola escola, int matricula, int serie, String turno, PontoDeParada pontoDeParada) {
         super(nome_oficial, cpf_cnpj, endereco, telefone, nome, mae, pai, naturalidade, data_nascimento);
-        this.cpf = cpf;
         this.escola = escola;
         this.matricula = matricula;
         this.serie = serie;
         this.turno = turno;
         this.pontoDeParada = pontoDeParada;
     }
-
-    public String getCpf() {
-        return this.cpf;
-    }
-
 
     public Escola getEscola() {
         return this.escola;
