ce1eddcb75ba9852bd90015e63e8275535e88e57
devruso <jamilsonjr@live.com>
2023-10-28 12:16:21 -0300

    schoolBus_feat: classe pessoa jurídica com construtor

---
+++ src/projeto_SchoolBus/PessoaJuridica.java
@@ -0,0 +1,17 @@
+package projeto_SchoolBus;
+
+public class PessoaJuridica extends Pessoa {
+
+    private String nome_fantasia;
+    private int num_funcionario;
+
+
+    public PessoaJuridica(
+            String nome_oficial, String cpf_cnpj, Endereco endereco, String telefone,
+            String nome_fantasia, int num_funcionario
+    ) {
+        super(nome_oficial, cpf_cnpj, endereco, telefone);
+        this.nome_fantasia = nome_fantasia;
+        this.num_funcionario = num_funcionario;
+    }
+}
